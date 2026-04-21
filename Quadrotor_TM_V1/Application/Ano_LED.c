b84a809d08438ee60d110fe55bb11b8c563bec8a
Rm3king <2712029723@qq.com>
2026-04-21 18:19:48 +0800

    refactor(stage4): normalize led task comments

--- Quadrotor_TM_V1/Application/Ano_LED.c
+++ Quadrotor_TM_V1/Application/Ano_LED.c
@@ -1,320 +1,392 @@
 #include "ANO_LED.h"
 #include "ANO_MATH.h"
 #include "Drv_led.h"
-
-
-u16 led_accuracy = 20;//该时间应与LED_Duty()调用周期相同
-float LED_Brightness[4] = {0,20,0,0}; //TO 20 //XBRG
-
-
-//LED的1ms驱动，在1ms定时中断里调用。
-void LED_1ms_DRV( ) //0~20
-{
-	static u16 led_cnt[4];
-	u8 i;
-	for(i=0;i<4;i++)
-	{
-			
-		if( led_cnt[i] < LED_Brightness[i] )
-		{
-			switch(i)
-			{
-				case 2:	
-					Drv_LedOnOff(LED_R, 1);//LED1_ON;
-				break;
-				case 3:	
-					Drv_LedOnOff(LED_G, 1);//LED2_ON;
-				break;
-				case 1:	
-					Drv_LedOnOff(LED_B, 1);//LED3_ON;
-				break;
-				case 0:	
-					Drv_LedOnOff(LED_S, 1);//LED4_ON;
-				break;
-			}
-		}
-		else
-		{
-			switch(i)
-			{
-				case 2:	
-					Drv_LedOnOff(LED_R, 0);//LED1_OFF;
-				break;
-				case 3:	
-					Drv_LedOnOff(LED_G, 0);//LED2_OFF;
-				break;
-				case 1:	
-					Drv_LedOnOff(LED_B, 0);//LED3_OFF;
-				break;
-				case 0:	
-					Drv_LedOnOff(LED_S, 0);//LED4_OFF;
-				break;
-			}
-		}
-		
-		if(++led_cnt[i]>=led_accuracy)
-		{
-			led_cnt[i] = 0;
-		}
-	}
-	
-
-}
-
-static void ledOnOff(u8 led)
-{
-	for(u8 i=0; i<LED_NUM; i++)
-	{
-		if(led & (1<<i))
-			LED_Brightness[i] = 20;
-		else
-			LED_Brightness[i] = 0;
-	}
-}
-static void ledBreath(u8 dT_ms,u8 led,u16 T)
-{
-	static u8 dir[LED_NUM];
-	
-	for(u8 i=0; i<LED_NUM; i++)
-	{
-		if(led & (1<<i))
-		{
-			switch(dir[i])
-			{
-				case 0:
-					LED_Brightness[i] += safe_div(led_accuracy,((float)T/(dT_ms)),0);
-					if(LED_Brightness[i]>20)
-					{
-						dir[i] = 1;
-					}
-				
-				break;
-				case 1:
-					LED_Brightness[i] -= safe_div(led_accuracy,((float)T/(dT_ms)),0);
-					if(LED_Brightness[i]<0)
-					{
-						dir[i] = 0;
-					}
-					
-				break;
-					
-				default:
-					dir[i] = 0;
-				break;
-			}
-		}
-		else
-			LED_Brightness[i] = 0;
-	}
-}
-//					调用周期   LED     亮时间    灭时间
-static void ledFlash(u8 dT_ms,u8 led, u16 on_ms,u16 off_ms)
-{
-	static u16 tim_tmp;
-	
-	if(tim_tmp < on_ms)
-		ledOnOff(led);
-	else
-		ledOnOff(0);
-	
-	tim_tmp += dT_ms;
-	if(tim_tmp >= (on_ms + off_ms))
-		tim_tmp = 0;
-}
-
-_led_sta LED_STA;
 #include "Ano_FcData.h"
 #include "Ano_OPMV_CBTracking_Ctrl.h"
 #include "Ano_OPMV_LineTracking_Ctrl.h"
-void LED_Task2(u8 dT_ms)  //11ms执行一次，用来实现LED的各类效果
+/*
+ * ?????
+ * LED ???????
+ *
+ * ????????????? LED ????????????????
+ * ??????????
+ */
+/* PWM ??????? LED_1ms_DRV() ???????? */
+u16 led_accuracy = 20;
+/* LED ???????? X?B?R?G? */
+float LED_Brightness[4] = {0, 20, 0, 0};
+/* ????????? */
+_led_sta LED_STA;
+/*
+ * ???1ms LED ???
+ * ???
+ * ???????????? PWM???????? led_accuracy ???
+ */
+void LED_1ms_DRV(void)
 {
-	static u16 timtmp = 0;
-
-	if(LED_STA.errOneTime)  //红色亮3s，表示错误一次
-	{
-		ledOnOff(BIT_RLED);
-		timtmp += dT_ms;
-		if(timtmp > 3000)
-		{
-			timtmp = 0;
-			LED_STA.errOneTime = 0;
-		}
-	}
-	else if(LED_STA.errMpu>0 || LED_STA.errMag>0 || LED_STA.errBaro>0)
-	{
-		u8 flashtims;
-		static u8 cnttmp = 0;
-		//传感器故障 	蓝色短闪+长间隔 	陀螺仪与加速度：快闪 2 次， 磁力计：快闪 3 次， 气压计：快闪 4 次
-		if(LED_STA.errMpu>0)
-			flashtims = 2;
-		else if(LED_STA.errMag>0)
-			flashtims = 3;
-		else 
-			flashtims = 4;
-		
-		if(cnttmp < flashtims)
-		{
-			if(timtmp < 60)
-				ledOnOff(BIT_BLED); //
-			else
-				ledOnOff(0);
-			timtmp += dT_ms;
-			
-			if(timtmp > 200)
-			{
-				timtmp = 0;
-				cnttmp++;
-			}
-		}
-		else
-		{
-			timtmp += dT_ms;
-			if(timtmp > 1000)
-			{
-				timtmp = 0;
-				cnttmp = 0;
-			}
-		}
-	}
-
-	else if(LED_STA.saving)  //数据保存中 绿色 在数据存储过程中，绿色常量
-	{
-		LED_Brightness[G_led] = 20;
-		LED_Brightness[R_led] = 0;
-		LED_Brightness[B_led] = 0;
-	}
-	else if(LED_STA.calAcc || LED_STA.calGyr || LED_STA.rst_imu)  //开机静止前 蓝色快闪 开机后默认状态，飞机正常初始化完毕，并静止后（尽量水平，但不必须），进入正常状态
-	{
-		ledFlash(dT_ms,BIT_BLED,40,40);
-	}
-	else if(LED_STA.calMag)  //磁力计校准指示
-	{
-		if(LED_STA.calMag == 1)
-			ledBreath(dT_ms,BIT_GLED,300);  
-		//盘校准步骤 2  绿色呼吸水平旋转提示，绿色呼吸期间，飞机水平端在胸前，人原地顺时针转 360 度，若过程出错，比如水平倾斜太大，会返回步骤 1
-		else if(LED_STA.calMag == 2)
-			ledFlash(dT_ms,BIT_BLED,40,40); //罗盘校准步骤 3 蓝色快闪 水平旋转完成标志，此时将飞机机头朝向天空，自动进入步骤 4
-		else if(LED_STA.calMag == 100)
-			ledFlash(dT_ms,BIT_GLED,40,40);  //罗盘校准步骤 1 绿色快闪 罗盘校准第一步提示，请将飞机放平，自动进入步骤 2
-		else
-			ledBreath(dT_ms,BIT_BLED,300);  //罗盘校准步骤 4 蓝色呼吸垂直旋转提示，蓝色呼吸期间，飞机机头朝上端在胸前，人原地逆时针转 360 度，若过程出错，比如没有保持机身垂直，会返回步骤 3
-			
-	}
-	else if(LED_STA.noRc)     //   失控 红色呼吸 遥控接收机异常，飞机进入失控状态   
-	{
-		ledBreath(dT_ms,BIT_RLED,500);
-	}
-	else if(LED_STA.lowVt)  //低压报警 红色短闪+短间隔 高频红色闪烁，表示电压低于报警电压
-	{
-		ledFlash(dT_ms,BIT_RLED,60,60);
-	}
-	else	//无其他提示，正常显示模式档位及外置光流、Gps等状态
-	{
-		static u8  statmp = 0;
-		static u8  modtmp = 0;
-		
-		if(statmp == 0)			//显示飞行模式1、2、3，白色未解锁，绿色解锁，模式几就闪几次
-		{
-			if(modtmp <= flag.flight_mode)
-			{
-				if(timtmp < 60)
-				{
-					if(flag.unlock_sta)
-						ledOnOff(BIT_GLED);//解锁状态：模式提示为绿色短闪，闪烁次数 1-3，分别代表飞行模式 1、 2、 3
-					else
-						ledOnOff(BIT_WLED);//未解锁状态：模式提示为白色短闪，闪烁次数 1-3，分别代表飞行模式 1、 2、 3
-				}
-				else
-					ledOnOff(0);
-				timtmp += dT_ms;
-				
-				if(timtmp > 200)
-				{
-					timtmp = 0;
-					modtmp++;
-				}
-			}
-			else
-			{
-				modtmp = 0;
-				statmp = 1;
-			}
-		}
-		else if(statmp == 1)	//显示光流、GPS等状态
-		{
-			if(modtmp == 0)		//判断GPS是否正常
-			{
-				if(switchs.gps_on)
-				{
-					if(timtmp < 60)
-						ledOnOff(BIT_BLED);
-					else
-						ledOnOff(0);
-					timtmp += dT_ms;
-					if(timtmp > 200)
-					{
-						timtmp = 0;
-						modtmp++;
-					}
-				}
-				else
-					modtmp = 1;
-			}
-			else if(modtmp == 1)	//判断光流是否正常
-			{
-				if(switchs.of_flow_on>0 && switchs.of_tof_on>0)
-				{
-					if(timtmp < 60)
-						ledOnOff(BIT_YLED);  //光流模块工作正常：模块提示为黄色单闪
-					else
-						ledOnOff(0);
-					timtmp += dT_ms;
-					if(timtmp > 200)
-					{
-						timtmp = 0;
-						modtmp++;
-					}
-				}
-				else
-					modtmp = 2;
-			}
-			else if(modtmp == 2)		//判断飞控外接ANO_OPMV是否正常
-			{
-				if(switchs.opmv_on && (ano_opmv_cbt_ctrl.target_loss==0 || ano_opmv_lt_ctrl.target_loss==0) )
-				{
-					if(timtmp < 60)
-						ledOnOff(BIT_PLED); //摄像头正常 ：模块提示为紫色单闪
-					else
-						ledOnOff(0);
-					timtmp += dT_ms;
-					if(timtmp > 200)
-					{
-						timtmp = 0;
-						modtmp++;
-					}
-				}
-				else
-					modtmp = 3;
-			}
-			
-			if(modtmp == 3)
-			{
-				statmp = 2;
-				modtmp = 0;
-			}
-		}
-		else		//每一轮循环后，加一个灭灯长延时
-		{
-			ledOnOff(0);
-			timtmp += dT_ms;
-			if(timtmp > 1000)
-			{
-				timtmp = 0;
-				statmp = 0;
-				modtmp = 0;
-			}
-		}
-	}
+    static u16 led_cnt[4];
+    u8 i;
+    for(i = 0; i < 4; i++)
+    {
+        if(led_cnt[i] < LED_Brightness[i])
+        {
+            switch(i)
+            {
+                case 2:
+                    Drv_LedOnOff(LED_R, 1);
+                break;
+                case 3:
+                    Drv_LedOnOff(LED_G, 1);
+                break;
+                case 1:
+                    Drv_LedOnOff(LED_B, 1);
+                break;
+                case 0:
+                    Drv_LedOnOff(LED_S, 1);
+                break;
+            }
+        }
+        else
+        {
+            switch(i)
+            {
+                case 2:
+                    Drv_LedOnOff(LED_R, 0);
+                break;
+                case 3:
+                    Drv_LedOnOff(LED_G, 0);
+                break;
+                case 1:
+                    Drv_LedOnOff(LED_B, 0);
+                break;
+                case 0:
+                    Drv_LedOnOff(LED_S, 0);
+                break;
+            }
+        }
+        if(++led_cnt[i] >= led_accuracy)
+        {
+            led_cnt[i] = 0;
+        }
+    }
 }
-
-
+/*
+ * ?????????????
+ * ???
+ * ???? LED ????????? LED ???
+ */
+static void ledOnOff(u8 led)
+{
+    u8 i;
+    for(i = 0; i < LED_NUM; i++)
+    {
+        if(led & (1 << i))
+        {
+            LED_Brightness[i] = 20;
+        }
+        else
+        {
+            LED_Brightness[i] = 0;
+        }
+    }
+}
+/*
+ * ?????????
+ * ???
+ * T ???????????? ms?
+ */
+static void ledBreath(u8 dT_ms, u8 led, u16 T)
+{
+    static u8 dir[LED_NUM];
+    u8 i;
+    for(i = 0; i < LED_NUM; i++)
+    {
+        if(led & (1 << i))
+        {
+            switch(dir[i])
+            {
+                case 0:
+                    LED_Brightness[i] += safe_div(led_accuracy, ((float)T / dT_ms), 0);
+                    if(LED_Brightness[i] > 20)
+                    {
+                        dir[i] = 1;
+                    }
+                break;
+                case 1:
+                    LED_Brightness[i] -= safe_div(led_accuracy, ((float)T / dT_ms), 0);
+                    if(LED_Brightness[i] < 0)
+                    {
+                        dir[i] = 0;
+                    }
+                break;
+                default:
+                    dir[i] = 0;
+                break;
+            }
+        }
+        else
+        {
+            LED_Brightness[i] = 0;
+        }
+    }
+}
+/*
+ * ?????????
+ * ???
+ * on_ms ??????off_ms ???????? ms?
+ */
+static void ledFlash(u8 dT_ms, u8 led, u16 on_ms, u16 off_ms)
+{
+    static u16 tim_tmp;
+    if(tim_tmp < on_ms)
+    {
+        ledOnOff(led);
+    }
+    else
+    {
+        ledOnOff(0);
+    }
+    tim_tmp += dT_ms;
+    if(tim_tmp >= (on_ms + off_ms))
+    {
+        tim_tmp = 0;
+    }
+}
+/*
+ * ???LED ???????
+ * ???
+ * ? 11ms ????????????????????????????
+ */
+void LED_Task2(u8 dT_ms)
+{
+    static u16 timtmp = 0;
+    if(LED_STA.errOneTime)
+    {
+        /* ??????????? 3 ?? */
+        ledOnOff(BIT_RLED);
+        timtmp += dT_ms;
+        if(timtmp > 3000)
+        {
+            timtmp = 0;
+            LED_STA.errOneTime = 0;
+        }
+    }
+    else if(LED_STA.errMpu > 0 || LED_STA.errMag > 0 || LED_STA.errBaro > 0)
+    {
+        u8 flashtims;
+        static u8 cnttmp = 0;
+        /* ????????????????? */
+        if(LED_STA.errMpu > 0)
+        {
+            flashtims = 2;
+        }
+        else if(LED_STA.errMag > 0)
+        {
+            flashtims = 3;
+        }
+        else
+        {
+            flashtims = 4;
+        }
+        if(cnttmp < flashtims)
+        {
+            if(timtmp < 60)
+            {
+                ledOnOff(BIT_BLED);
+            }
+            else
+            {
+                ledOnOff(0);
+            }
+            timtmp += dT_ms;
+            if(timtmp > 200)
+            {
+                timtmp = 0;
+                cnttmp++;
+            }
+        }
+        else
+        {
+            timtmp += dT_ms;
+            if(timtmp > 1000)
+            {
+                timtmp = 0;
+                cnttmp = 0;
+            }
+        }
+    }
+    else if(LED_STA.saving)
+    {
+        /* ???????????? */
+        LED_Brightness[G_led] = 20;
+        LED_Brightness[R_led] = 0;
+        LED_Brightness[B_led] = 0;
+    }
+    else if(LED_STA.calAcc || LED_STA.calGyr || LED_STA.rst_imu)
+    {
+        /* ????? IMU ?????????? */
+        ledFlash(dT_ms, BIT_BLED, 40, 40);
+    }
+    else if(LED_STA.calMag)
+    {
+        /* ??????????/?????????????? */
+        if(LED_STA.calMag == 1)
+        {
+            ledBreath(dT_ms, BIT_GLED, 300);
+        }
+        else if(LED_STA.calMag == 2)
+        {
+            ledFlash(dT_ms, BIT_BLED, 40, 40);
+        }
+        else if(LED_STA.calMag == 100)
+        {
+            ledFlash(dT_ms, BIT_GLED, 40, 40);
+        }
+        else
+        {
+            ledBreath(dT_ms, BIT_BLED, 300);
+        }
+    }
+    else if(LED_STA.noRc)
+    {
+        /* ?????????? */
+        ledBreath(dT_ms, BIT_RLED, 500);
+    }
+    else if(LED_STA.lowVt)
+    {
+        /* ???????????? */
+        ledFlash(dT_ms, BIT_RLED, 60, 60);
+    }
+    else
+    {
+        static u8 statmp = 0;
+        static u8 modtmp = 0;
+        /* ??????????????????? */
+        if(statmp == 0)
+        {
+            /* ??????????????????????????? */
+            if(modtmp <= flag.flight_mode)
+            {
+                if(timtmp < 60)
+                {
+                    if(flag.unlock_sta)
+                    {
+                        ledOnOff(BIT_GLED);
+                    }
+                    else
+                    {
+                        ledOnOff(BIT_WLED);
+                    }
+                }
+                else
+                {
+                    ledOnOff(0);
+                }
+                timtmp += dT_ms;
+                if(timtmp > 200)
+                {
+                    timtmp = 0;
+                    modtmp++;
+                }
+            }
+            else
+            {
+                modtmp = 0;
+                statmp = 1;
+            }
+        }
+        else if(statmp == 1)
+        {
+            /* ?????????GPS????OpenMV? */
+            if(modtmp == 0)
+            {
+                if(switchs.gps_on)
+                {
+                    if(timtmp < 60)
+                    {
+                        ledOnOff(BIT_BLED);
+                    }
+                    else
+                    {
+                        ledOnOff(0);
+                    }
+                    timtmp += dT_ms;
+                    if(timtmp > 200)
+                    {
+                        timtmp = 0;
+                        modtmp++;
+                    }
+                }
+                else
+                {
+                    modtmp = 1;
+                }
+            }
+            else if(modtmp == 1)
+            {
+                if(switchs.of_flow_on > 0 && switchs.of_tof_on > 0)
+                {
+                    if(timtmp < 60)
+                    {
+                        ledOnOff(BIT_YLED);
+                    }
+                    else
+                    {
+                        ledOnOff(0);
+                    }
+                    timtmp += dT_ms;
+                    if(timtmp > 200)
+                    {
+                        timtmp = 0;
+                        modtmp++;
+                    }
+                }
+                else
+                {
+                    modtmp = 2;
+                }
+            }
+            else if(modtmp == 2)
+            {
+                if(switchs.opmv_on && (ano_opmv_cbt_ctrl.target_loss == 0 || ano_opmv_lt_ctrl.target_loss == 0))
+                {
+                    if(timtmp < 60)
+                    {
+                        ledOnOff(BIT_PLED);
+                    }
+                    else
+                    {
+                        ledOnOff(0);
+                    }
+                    timtmp += dT_ms;
+                    if(timtmp > 200)
+                    {
+                        timtmp = 0;
+                        modtmp++;
+                    }
+                }
+                else
+                {
+                    modtmp = 3;
+                }
+            }
+            if(modtmp == 3)
+            {
+                statmp = 2;
+                modtmp = 0;
+            }
+        }
+        else
+        {
+            /* ???????????????? */
+            ledOnOff(0);
+            timtmp += dT_ms;
+            if(timtmp > 1000)
+            {
+                timtmp = 0;
+                statmp = 0;
+                modtmp = 0;
+            }
+        }
+    }
+}
